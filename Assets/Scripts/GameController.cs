c58dfca36c2512da30e8eadc71aab2879fee728c
Roberto Mario Buelvas <robertobuelvas94@gmail.com>
2019-03-02 13:16:33 -0500

    Added an end to the game and restart

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
@@ -14,9 +15,16 @@ public class GameController : MonoBehaviour
     public GameObject spawnUp;
     public GameObject spawnMiddle;
     public GameObject spawnDown;
+    public Text gOText;
+
+    public float maxTime;
+    public float waitTime;
 
     private int lives;
     private int score;
+    private float timer = 0f;
+    private int counter = 0;
+    private bool gameOverFlag = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +32,18 @@ public class GameController : MonoBehaviour
         lives = 3;
         score = 0;
         Instantiate(targetBlue, spawnMiddle.transform);
+        counter++;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         if (lives <= 0)
         {
-            //Restart the game
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            gOText.text = "Try again \n Your score was " + score;
+            gOText.gameObject.SetActive(true);
+            gameOverFlag = true;
         }
 
         if (Input.GetKey("a"))
@@ -45,7 +56,21 @@ public class GameController : MonoBehaviour
             colorWheel.transform.Rotate(new Vector3(0, 0, -10));
         }
 
-        
+        if(Input.GetKeyDown("enter") || Input.GetKeyDown("space"))
+        {
+            if (gameOverFlag)
+            {
+                //Restart the game
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        if (timer >= maxTime && gameOverFlag==false)
+        {
+            gOText.text = "Congratulations! You won! \n Your score was " + score;
+            gOText.gameObject.SetActive(true);
+            gameOverFlag = true;
+        }
+        UpdateSpawning();        
     }
 
     public void IncrementScore()
@@ -67,4 +92,19 @@ public class GameController : MonoBehaviour
     {
         return score;
     }
+
+    void UpdateSpawning()
+    {
+        if(timer >= 2 && counter == 1)
+        {
+            Instantiate(targetGreen, spawnDown.transform);
+            counter++;
+        }
+        if (timer >= 4 && counter == 2)
+        {
+            Instantiate(targetYellow, spawnUp.transform);
+            counter++;
+        }
+    }
+
 }
