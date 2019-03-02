c968a24c471ba18c17916e93e6c4ea3ced07ec1e
Roberto Mario Buelvas <robertobuelvas94@gmail.com>
2019-03-02 11:04:44 -0500

    Added script to rotate

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public GameController gc;
+    public GameObject coloredCube;
+
+    private Color myColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myColor = coloredCube.GetComponent<Renderer>().material.GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            Color otherColor = other.GetComponent<Renderer>().material.GetColor("_Color");
+            if (otherColor == myColor)
+            {
+                gc.IncrementScore();
+            }
+            else
+            {
+                gc.LoseLife();
+            }
+            Destroy(other);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject colorWheel;
+
+    private int lives;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 3;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives <= 0)
+        {
+            //Restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            colorWheel.transform.Rotate(new Vector3(0, 0, 10));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            colorWheel.transform.Rotate(new Vector3(0, 0, -10));
+        }
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+    }
+}
